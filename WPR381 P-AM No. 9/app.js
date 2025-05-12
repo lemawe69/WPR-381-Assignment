dce8fe67aeac52a53592d4c8bd330fa6a366d1c6
JPgitstuff <jpseaman0001@gmail.com>
2025-05-12 12:17:29 +0200

    Member 2 upodate thank you

--- WPR381 P-AM No. 9/app.js
+++ WPR381 P-AM No. 9/app.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
--- WPR381 P-AM No. 9/routes/pageRoutes.js
+++ WPR381 P-AM No. 9/routes/pageRoutes.js
@@ -3,19 +3,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+
+// const teamMembers = [
+//   { name: "Jan-Paul", role: "Backend Developer" }
+// ];
+// const events = [
+//   { title: "Tech Expo", date: "2025-06-15", location: "Cape Town" }
+// ];
+
+let messages = [];
+
+router.get('/', (req, res) => { 
+    res.render("pages/home")
 });
 
 router.get('/about', (req, res) => {
+    res.render('pages/about')
 });
 
 router.get('/events', (req, res) => {
+    res.render('pages/events')
 });
 
 router.get('/contact', (req, res) => {
+    res.render('pages/contact')
 });
-
-router.get('/thankyou', (req, res) => {
+router.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    messages.push({name, email, message});
+    res.redirect(`/thankyou?name=${encodeURIComponent(name)}`);
+});
+router.get('/thankyou', (req, res) => { 
+    const { name } = req.query;
+    res.render ('pages/thankyou', {name})
 });
 
 module.exports = router;
