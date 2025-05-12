25aab850910b63bf89a40325cf45592fa1f62e58
lemawe69 <brandonlemmer777@gmail.com>
2025-05-12 09:50:10 +0200

    Member 1 Update

--- WPR381 P-AM No. 9/routes/pageRoutes.js
+++ WPR381 P-AM No. 9/routes/pageRoutes.js
@@ -4,7 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.render("pages/home", {titel: "GameDEV Home Page"})
 });
+
 
 router.get('/about', (req, res) => {
 });
